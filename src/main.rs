6e122461e5bdaaade1e1d1750cb8eceffe053844
Suard Damme <suardd@gmail.com>
2025-04-12 23:07:55 +0200

    added message option

--- src/main.rs
+++ src/main.rs
@@ -21,24 +21,33 @@ fn main() -> Result<(), anyhow::Error> {
     println!("target commit: {}", target_commit.id().to_string());
 
     let parent_commit = get_nth_parent(&head_commit, number_of_commits_between - 1).unwrap();
-    let commit_message = parent_commit.message().unwrap();
+    // let commit_message = ;
+    
+    let commit_message = match args.message {
+        None => {
+            parent_commit.message().unwrap().to_string()
+        }
+        Some(msg) => {
+            msg.to_string()
+        }
+    };
     
     println!("commit message: {}", commit_message);
     
     if args.dry == false {
         repository.reset(target_commit.as_object(), ResetType::Soft, None)?;
-
+    
         let mut index = repository.index()?;
         index.add_all(["*"].iter(), IndexAddOption::DEFAULT, None)?;
         index.write()?;
-
+    
         let branch_ref = current_branch.name().unwrap();
-
+    
         let sig = repository.signature()?;
         let tree_id = index.write_tree()?;
         let tree = repository.find_tree(tree_id)?;
-        repository.commit(Some(branch_ref), &sig, &sig, commit_message, &tree, &[&target_commit])?;
-
+        repository.commit(Some(branch_ref), &sig, &sig, &commit_message, &tree, &[&target_commit])?;
+    
         println!("commit done");
     }
     
@@ -94,6 +103,12 @@ struct Args {
         help = "The main branch name eg main, master etc"
     )]
     main: String,
+    #[arg(
+        short = 'a',
+        long,
+        help = "Override the commit message of the squashed commit"
+    )]
+    message: Option<String>,
     #[arg(long, help = "Initiate dry run and not executing the squash")]
     dry: bool,
 }
